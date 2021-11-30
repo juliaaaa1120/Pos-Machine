9b7b7dc684df2bcd96cb570f4773c7ad68f8c067
Julia Ng <ngju@oocl.com>
2021-11-30 20:32:08 +0800

    Built function - loadAllItemInfos

--- src/main/java/pos/machine/ItemDataLoader.java
+++ src/main/java/pos/machine/ItemDataLoader.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ItemDataLoader {
-    public static List<ItemInfo> loadAllItemInfos(List<ItemInfo> itemInfos) {
+    public static List<ItemInfo> loadAllItemInfos(List<ItemInfo> database) {
         ItemInfo item1Info = new ItemInfo("ITEM000000", "Coca-Cola", 3);
         ItemInfo item2Info = new ItemInfo("ITEM000001", "Sprite", 3);
         ItemInfo item3Info = new ItemInfo("ITEM000004", "Battery", 2);
-        itemInfos.add(item1Info);
-        itemInfos.add(item2Info);
-        itemInfos.add(item3Info);
+        database.add(item1Info);
+        database.add(item2Info);
+        database.add(item3Info);
 
-        return itemInfos;
+        return database;
     }
 
     public static List<String> loadBarcodes() {
