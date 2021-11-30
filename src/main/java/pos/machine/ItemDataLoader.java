115e9a616c8b7cffd673310342e0efac2c9bd033
Julia Ng <ngju@oocl.com>
2021-11-30 20:29:06 +0800

    Built function - loadAllItemInfos

--- src/main/java/pos/machine/ItemDataLoader.java
+++ src/main/java/pos/machine/ItemDataLoader.java
@@ -5,11 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ItemDataLoader {
-    public static List<ItemInfo> loadAllItemInfos() {
+    public static List<ItemInfo> loadAllItemInfos(List<ItemInfo> itemInfos) {
         ItemInfo item1Info = new ItemInfo("ITEM000000", "Coca-Cola", 3);
         ItemInfo item2Info = new ItemInfo("ITEM000001", "Sprite", 3);
         ItemInfo item3Info = new ItemInfo("ITEM000004", "Battery", 2);
-        List<ItemInfo> itemInfos = new ArrayList<>();
         itemInfos.add(item1Info);
         itemInfos.add(item2Info);
         itemInfos.add(item3Info);
